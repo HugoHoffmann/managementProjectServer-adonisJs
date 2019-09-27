53fc98e3cdd658e71378d9a550096303fc259a19
Hugo <hugohoffmann04@gmail.com>
2019-09-27 09:19:34 -0300

    chore: create seed

---
+++ backend/database/seeds/DatabaseSeeder.js
@@ -0,0 +1,23 @@
+'use strict'
+
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+const User = use('App/Models/User')
+class DatabaseSeeder {
+  async run () {
+    const user = await User.create({
+      name: 'Hugo Hoffmann',
+      email: 'hugohoffmann04@gmail.com',
+      password: '123456'
+    })
+
+    await user.teams().create({
+      name: 'Htech',
+      user_id: user.id,
+      
+    })
+  }
+}
+
+module.exports = DatabaseSeeder

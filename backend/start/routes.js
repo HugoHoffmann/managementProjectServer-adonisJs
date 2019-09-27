0d2e9285b6fd49d7fbd0c42cd2a509da25109e38
Hugo <hugohoffmann04@gmail.com>
2019-09-27 16:38:36 -0300

    new: create CRUD projects

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -25,4 +25,7 @@ Route.group( () => {
 
 Route.group( () => {
   Route.post('invites', 'InviteController.store')
+
+  Route.resource('projects', 'ProjectController').apiOnly()
+
 }).middleware([ 'auth', 'team' ])
